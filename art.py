1ff86aa1aaead33f771dc084d36e5f5fb370ed5e
safimuhammad <safiwahid38@gmail.com>
2022-09-19 01:31:27 +0500

    checking the  grey scale

--- art.py
+++ art.py
@@ -1,1 +1,14 @@
-#initial commit
+from PIL import Image
+import math
+
+
+# 70 levels of gray
+gscale1 = '"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`". '
+# 10 levels of gray
+gscale2 = "@%#*+=-:. "
+
+image = Image.open('test.JPG').convert('L')
+W,H = image.size[0],image.size[1]
+
+
+print(W,H)
